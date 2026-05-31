26cfea561d0983c3945daebf1e52970a2739db1e
ihsan-cavos <rafi.ihsan@cavos.io>
2026-05-31 22:59:42 +0700

    feat: add Cloudflare Workers types to devDependencies and configure Nitro preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+    nitro: {
+    preset: 'cloudflare-pages'
+  },
   components:[{
       path: '~/components',
       pathPrefix: true,
