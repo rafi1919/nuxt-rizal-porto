c9c8efb47345948249dbfe62f1224dcf580ee7e6
ihsan-cavos <rafi.ihsan@cavos.io>
2026-06-01 09:56:00 +0700

    feat: add baseURL configuration for app in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    baseURL: '/nuxt-rizal-porto/',
+  },
    
   nitro: {
     preset: 'static',
